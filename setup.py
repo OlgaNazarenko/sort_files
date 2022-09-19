32d7c272540350b65483201eb02a830707cd64aa
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2022-09-19 19:16:53 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='sorting_folder',
+    version='1.0',
+    description='Sorting of the folder',
+    url='https://github.com/olganazarenko/Clean_folder.git',
+    author='The lost person in Python',
+    author_email='olga.nazarenko31@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages()
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
+)
